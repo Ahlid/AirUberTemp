39a1e45da37d4f96ee22b32c980ee0849471e1b4
Ricardo Morais <moraispgsi@gmail.com>
2017-01-10 20:45:24 +0000

    Falta tratar do número de viagens, comentar e talvez alguns retoques.

--- AirUberProjeto/src/AirUberProjeto/Controllers/ClienteController.cs
+++ AirUberProjeto/src/AirUberProjeto/Controllers/ClienteController.cs
@@ -20,10 +20,11 @@ namespace AirUberProjeto.Controllers
     [Authorize(Roles = Roles.ROLE_CLIENTE)]
     public class ClienteController : Controller
     {
-
+        
+        /// <summary>
+        /// Utilizado para sabermos o caminho absoluto do servidor
+        /// </summary>
         private IHostingEnvironment _environment;
-
-
 
         /// <summary>
         /// O contexto da aplicação para poder aceder a dados.
@@ -37,6 +38,12 @@ namespace AirUberProjeto.Controllers
 
 
         
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="userManager"></param>
+        /// <param name="environment"></param>
         public ClienteController(AirUberDbContext context, UserManager<ApplicationUser> userManager, IHostingEnvironment environment)
         { 
             _environment = environment;
@@ -44,6 +51,10 @@ namespace AirUberProjeto.Controllers
             _userManager = userManager;
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
         public IActionResult Index()
         {
@@ -52,6 +63,10 @@ namespace AirUberProjeto.Controllers
 
          
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
         public IActionResult Perfil()
         {
@@ -74,6 +89,36 @@ namespace AirUberProjeto.Controllers
             return View(viewModel);
         }
 
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public bool MarcarNotificacaoLida(int id)
+        {
+            try
+            {
+                Notificacao notificacao = _context.Notificacao.First(n => n.NotificacaoId == id);
+                notificacao.Lida = true;
+                _context.Notificacao.Update(notificacao);
+                _context.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
         public IActionResult EditarPerfil()
         {
@@ -82,39 +127,64 @@ namespace AirUberProjeto.Controllers
             return View(cliente);
         }
 
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="viewModel"></param>
+        /// <returns></returns>
         [HttpPost]
         public IActionResult EditarPerfil(EditarPerfilViewModel viewModel)
         {
             if (ModelState.IsValid)
             {
-                Cliente cliente = (Cliente)_userManager.GetUserAsync(this.User).Result;
+                Cliente cliente = (Cliente) _userManager.GetUserAsync(this.User).Result;
                 cliente.Nome = viewModel.Nome;
                 cliente.Apelido = viewModel.Apelido;
                 cliente.Contacto = viewModel.Contacto;
                 _context.Update(cliente);
                 _context.SaveChanges();
+                ViewData["Success"] = true;
+                return View(cliente);
             }
             
-
-            return RedirectToAction(nameof(ClienteController.EditarPerfil), "Cliente");
+             return RedirectToAction(nameof(ClienteController.EditarPerfil), "Cliente");
+            
         }
 
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="file"></param>
+        /// <returns></returns>
         [HttpPost]
         public async Task<IActionResult> EditarImagemPerfil (IFormFile file)
         {
+
+            string extension = Path.GetExtension(file.FileName);
+            if (extension != ".jpg" && extension != ".png")
+                return null;
+
             Cliente cliente = (Cliente)_userManager.GetUserAsync(this.User).Result;
-            string dirPath = Path.Combine(_environment.WebRootPath, "clientes", "client-" + cliente.Email);
-            string path = Path.Combine(dirPath, "imagem-perfil" + Path.GetExtension(file.FileName));
 
-            Directory.CreateDirectory(dirPath);
+            string fileName = "imagem-perfil" + extension;
+            string folderName = Path.Combine("clientes", "client-" + cliente.Email);
+            string relativePathToFile = Path.Combine(folderName, fileName);
 
-            using (var fileStream = new FileStream(path, FileMode.Create))
+            string forderPath = Path.Combine(_environment.WebRootPath, folderName);
+            string absolutePathToFile = Path.Combine(_environment.WebRootPath, relativePathToFile);
+
+            //Cria a directoria caso ainda não exista
+            Directory.CreateDirectory(forderPath);
+            
+            //Transmite o ficheiro através de um FileStream
+            using (var fileStream = new FileStream(absolutePathToFile, FileMode.Create))
             {
                 await file.CopyToAsync(fileStream);
             }
 
-            cliente.RelativePathImagemPerfil = Path.Combine("clientes", "client-" + cliente.Email, "imagem-perfil" + Path.GetExtension(file.FileName));
+            cliente.RelativePathImagemPerfil = relativePathToFile;
             _context.Update(cliente);
             _context.SaveChanges();
 
